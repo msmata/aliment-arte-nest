bdf227507e3956110755e0d823c9f10a062f8673
ElMarto <martin@mata.com>
2023-04-11 12:01:31 -0300

    Remueve colacion y merienda de lista de momentos del dia

--- src/momentos/momentos.service.ts
+++ src/momentos/momentos.service.ts
@@ -5,11 +5,9 @@ import { Momento } from './momento.entity';
 export class MomentosService {
   getMomentos(): Momento[] {
     return [
-      { id: '1', name: 'Desayuno' },
+      { id: '1', name: 'Desayuno/Merienda' },
       { id: '2', name: 'Almuerzo' },
-      { id: '3', name: 'Merienda' },
       { id: '4', name: 'Cena' },
-      { id: '5', name: 'Colación' },
     ];
   }
 }
